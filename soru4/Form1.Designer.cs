fe28de7a4c06903650565263d51738abacc1885c
ANB-LAB <ANB-LAB@ANB-LAB4-BL4>
2023-12-06 14:08:26 +0300

    Proje dosyası ekle.

---
+++ soru4/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace soru4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_skor = new System.Windows.Forms.Label();
+            this.pb_top = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_top)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(296, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SKOR :";
+            // 
+            // lbl_skor
+            // 
+            this.lbl_skor.AutoSize = true;
+            this.lbl_skor.Location = new System.Drawing.Point(345, 9);
+            this.lbl_skor.Name = "lbl_skor";
+            this.lbl_skor.Size = new System.Drawing.Size(13, 13);
+            this.lbl_skor.TabIndex = 1;
+            this.lbl_skor.Text = "--";
+            // 
+            // pb_top
+            // 
+            this.pb_top.Image = ((System.Drawing.Image)(resources.GetObject("pb_top.Image")));
+            this.pb_top.Location = new System.Drawing.Point(115, 106);
+            this.pb_top.Name = "pb_top";
+            this.pb_top.Size = new System.Drawing.Size(50, 50);
+            this.pb_top.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_top.TabIndex = 2;
+            this.pb_top.TabStop = false;
+            this.pb_top.Click += new System.EventHandler(this.pb_top_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.pb_top);
+            this.Controls.Add(this.lbl_skor);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "SORU4";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_top)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_skor;
+        private System.Windows.Forms.PictureBox pb_top;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ soru4/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace soru4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random r = new Random();
+        int sayac;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+            timer1.Start();
+            
+
+           
+
+
+        }
+        
+        private void pb_top_Click(object sender, EventArgs e)
+        {
+            sayac++;
+            lbl_skor.Text = sayac.ToString();
+            if (sayac==10)
+            {
+                timer1.Stop();
+                MessageBox.Show("Tebrikle Oyunu Kazandınız");
+                Application.Exit();
+            }
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pb_top.Top = r.Next(0, 350);
+            pb_top.Left = r.Next(0, 350);
+            
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            sayac--;
+            lbl_skor.Text = sayac.ToString();
+            if (sayac == 10)
+            {
+                timer1.Stop();
+                MessageBox.Show("Tebrikle Oyunu Kazandınız");
+                Application.Exit();
+
+            }
+            
+
+        }
+    }
+}
